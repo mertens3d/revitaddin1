c7503930ee5e531f962eb99b5b3574798fcc8c2d
mertens3d <info@mertens3d.com>
2015-11-09 20:18:12 -0600

    Localization works

---
+++ Form1.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Forms;
+
+namespace RevitAddin1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            //  Thread.CurrentThread.CurrentUICulture = new CultureInfo("de");
+            InitializeComponent();
+        }
+    }
+}
